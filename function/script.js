d30ecacd969fe92c2cfb28ed8c9cb2eb3e7af933
Abel Akmal <abelakmal06@gmail.com>
2023-08-26 08:30:49 +0700

    fixed

--- function/script.js
+++ function/script.js
@@ -1,25 +1,22 @@
-const a = document.querySelectorAll('nav ul li a');
-const line = document.querySelectorAll('nav ul li .line');
-console.log(a[0].id);
+const aTag = document.querySelectorAll('nav ul li a');
+const line = document.querySelectorAll('nav ul .line');
 
-for (let i = 0; i < a.length; i++) {
-  a[i].addEventListener('click', function () {
-    var current = document.getElementsByClassName('active');
+for (let i = 0; i < aTag.length; i++) {
+  aTag[i].addEventListener('click', function () {
+    const current = document.getElementsByClassName('active');
     if (current.length > 0) {
       current[0].className = current[0].className.replace(' active', '');
     }
     this.className += ' active';
   });
 
-  if (a[i].id != 'a1') {
-    a[i].addEventListener('click', function () {
+  if (aTag[i].id != 'a1') {
+    aTag[i].addEventListener('click', function () {
       document.getElementById('a1').classList.remove('active');
     });
   }
 
-  a[i].addEventListener('mouseout', function () {
+  aTag[i].addEventListener('mouseout', function () {
     line[i].style.transition = '0.5s';
   });
 }
-
-console.log("test");
